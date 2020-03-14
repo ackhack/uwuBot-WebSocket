6f4dacf7693ac28d82fba51b7669ab6bc856ecf7
ackhack <christoph.feurbacher@gmx.de>
2020-03-14 17:53:36 +0100

    0.0.2

---
+++ README.md
@@ -0,0 +1,2 @@
+node modules:
+node-osu leagueapiwrapper snoowrap
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 60001 });
+const snoowrap = require('snoowrap');
+const apiKey = require('./Dependencies/RedditAPI.json');
+const redditAPI = new snoowrap({
+    userAgent: 'my user-agent',
+    clientId: apiKey.clientId,
+    clientSecret: apiKey.clientSecret,
+    username: apiKey.username,
+    password: apiKey.password
+});
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: ' + message);
-  });
+    let contentArgs = message.split(" "); //Split Message for simpler Access
+    switch (contentArgs[0]) {
+        case 'Reddit':
+            redditAPI.getSubreddit(contentArgs[1]).getRandomSubmission().then(submission => {
 
-  ws.send('Answer for' + message);
+                ws.send(JSON.stringify(submission));
+
+            }).catch(error => {
+                ws.send('ERROR');
+                console.log(error);
+            });
+            break;
+    }
+  });
 });
