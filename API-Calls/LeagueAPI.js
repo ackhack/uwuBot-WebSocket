be834647c6863ca4f3ea6bd681eac5cdafc21253
danieltheil <uwubot@LEFTDOGE.local>
2020-10-09 14:53:47 +0200

    2.3.1

--- API-Calls/LeagueAPI.js
+++ API-Calls/LeagueAPI.js
@@ -24,75 +24,76 @@ module.exports = {
 
         let name = args.substring(args.indexOf(' ') + 1);
 
-        let user = await api.Summoner.getByName(name, 'EUW1').catch(error => {
+        await api.Summoner.getByName(name, 'EUW1').catch(_ => {
             ws.send('ERROR: Summoner not found');
-            return;
-        });
+        }).then(async user => {
 
-        if (!user.response) {
-            ws.send('ERROR: Summoner not found');
-            return;
-        }
-
-        let currentMatch = await api.Spectator.activeGame(user.response.id, 'EUW1').catch(error => {
-            ws.send('ERROR: No active Game');
-            return;
-        });
-
-        if (!currentMatch.response) {
-            ws.send('ERROR: No active Game');
-            return;
-        }
-
-        if (SavedGames[currentMatch.response.gameId]) { //Skip API-Calls below if game is already saved
-            ws.send(JSON.stringify(SavedGames[currentMatch.response.gameId]));
-        }
-
-        let gameInfo = '';
-        if (currentMatch.response.gameMode == 'CLASSIC') {
-            gameInfo = 'Normal or Ranked Game on ';
-        } else {
-            gameInfo = gameInfo.concat(gameModes.find(mode => mode.gameMode == currentMatch.response.gameMode)).concat(' on ');
-        }
-
-        let map = maps.find(mode => mode.mapId == currentMatch.response.mapId);
-
-        gameInfo = gameInfo.concat(map['mapName'] + ' (' + map['notes'] + ')');
-
-
-        let Players = [];
-
-        for (let par of currentMatch.response.participants) {
-
-            let champ = twisted.Constants.Champions[par.championId];
-            champ = champ.charAt(0) + champ.substring(1, champ.length).toLowerCase(); //First Letter UpperCase
-            champ = champ.replace(/_(.)/g, function (m) { return ' ' + m.charAt(1).toUpperCase() }); //Replace '_l' with ' L'
-
-            let pl = await api.Summoner.getByName(par.summonerName, 'EUW1').catch(_ => {
-                ws.send('ERROR: Name not found');
+            if (!user)
                 return;
-            });;
-            let px = await api.League.bySummoner(par.summonerId, 'EUW1').catch(_ => {
-                ws.send('ERROR: Summoner not found');
-                return;
-            });;
 
-            var rank = 'Unranked';
-            if (px.response[0] != undefined) {
-                rank = px.response[0].tier.charAt(0) + px.response[0].tier.substring(1, px.response[0].tier.length).toLowerCase() + ' ' + px.response[0].rank;
-            }
+            await api.Spectator.activeGame(user.response.id, 'EUW1').catch(_ => {
+                ws.send('ERROR: No active Game');
+            }).then(async currentMatch => {
 
-            Players.push(new Player(par.summonerName, champ, rank, 'lvl ' + pl.response.summonerLevel))
-        }
+                if (!currentMatch)
+                    return;
 
-        let game = [currentMatch.response.gameId, Players, name, gameInfo];
+                if (!currentMatch.response) {
+                    ws.send('ERROR: No active Game');
+                    return;
+                }
 
-        SavedGames[currentMatch.response.gameId] = game;
+                if (SavedGames[currentMatch.response.gameId]) { //Skip API-Calls below if game is already saved
+                    ws.send(JSON.stringify(SavedGames[currentMatch.response.gameId]));
+                }
 
-        setTimeout(_ => { //Delete game after 2 hours
-            SavedGames[currentMatch.response.gameId] = undefined;
-        }, 7_200_000)
+                let gameInfo = '';
+                if (currentMatch.response.gameMode == 'CLASSIC')
+                    gameInfo = 'Normal or Ranked Game on ';
+                else
+                    gameInfo = gameInfo.concat(gameModes.find(mode => mode.gameMode == currentMatch.response.gameMode)).concat(' on ');
 
-        ws.send(JSON.stringify(game));
+
+                let map = maps.find(mode => mode.mapId == currentMatch.response.mapId);
+
+                gameInfo = gameInfo.concat(map['mapName'] + ' (' + map['notes'] + ')');
+
+
+                let Players = [];
+
+                for (let par of currentMatch.response.participants) {
+
+                    let champ = twisted.Constants.Champions[par.championId];
+                    champ = champ.charAt(0) + champ.substring(1, champ.length).toLowerCase(); //First Letter UpperCase
+                    champ = champ.replace(/_(.)/g, function (m) { return ' ' + m.charAt(1).toUpperCase() }); //Replace '_l' with ' L'
+
+                    let pl = await api.Summoner.getByName(par.summonerName, 'EUW1').catch(_ => {
+                        ws.send('ERROR: Name not found');
+                        return;
+                    });;
+                    let px = await api.League.bySummoner(par.summonerId, 'EUW1').catch(_ => {
+                        ws.send('ERROR: Summoner not found');
+                        return;
+                    });;
+
+                    var rank = 'Unranked';
+                    if (px.response[0] != undefined) {
+                        rank = px.response[0].tier.charAt(0) + px.response[0].tier.substring(1, px.response[0].tier.length).toLowerCase() + ' ' + px.response[0].rank;
+                    }
+
+                    Players.push(new Player(par.summonerName, champ, rank, 'lvl ' + pl.response.summonerLevel))
+                }
+
+                let game = [currentMatch.response.gameId, Players, name, gameInfo];
+
+                SavedGames[currentMatch.response.gameId] = game;
+
+                setTimeout(_ => { //Delete game after 2 hours
+                    SavedGames[currentMatch.response.gameId] = undefined;
+                }, 7_200_000)
+
+                ws.send(JSON.stringify(game));
+            });
+        });
     }
 }
