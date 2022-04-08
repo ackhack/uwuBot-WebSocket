705992ccfcbc17e4ef6af609ef6ad267c2ef791b
ackhack <christoph.fuerbacher@gmx.de>
2022-04-08 17:32:50 +0200

    osu api v2

--- API-Calls/osuAPI.js
+++ API-Calls/osuAPI.js
@@ -1,8 +1,7 @@
-const { Api } = require('node-osu');
+const { VoiceState } = require('discord.js');
+const { v2, auth } = require('osu-api-extended')
 const KEYFILE = '../Dependencies/osuAPIKey.json';
 let valid = false;
-let osu_api = undefined;
-
 module.exports = {
     osuAPI: function (ws, args) {
 
@@ -18,60 +17,32 @@ module.exports = {
         switch (contentArgs[1]) {
 
             case 'plays':
-                osu_api.getUserBest({ u: name }).then(
-                    scores => {
-                        var AccArray = [];
-                        for (let s of scores) {
-                            AccArray.push(s.accuracy);
-                        }
-
-                        ws.send(JSON.stringify([scores, AccArray]));
-                    }).catch((error) => {
-                        ws.send('ERROR: User not found or has no top plays.');
-                        console.log(error);
-                    });
+                v2.user.get(name, "osu").then(user => {
+                    v2.scores.users.best(user.id, { mode: "osu", limit: 5 }).then(scores => {
+                        ws.send(JSON.stringify(scores));
+                    })
+                })
                 break;
 
             case 'recent':
-                osu_api.getUserRecent({ u: name }).then(
-                    result => {
-                        ws.send(JSON.stringify([result[0], parseMods(result[0].mods), result[0].accuracy]));
-                    }
-                ).catch((error) => {
-                    ws.send('ERROR: User not found or has no recent plays.');
-                    console.log(error);
-                });
+                v2.user.get(name, "osu").then(user => {
+                    v2.scores.users.recent(user.id, { mode: "osu", limit: 1 }).then(scores => {
+                        ws.send(JSON.stringify(scores));
+                    })
+                })
                 break;
         }
     }
-}
-
-function parseMods(mods) {
-    let result = "";
-    for (let x = 0; x < mods.length; x++) {
-
-        if (mods[x] != 'FreeModAllowed' && mods[x] != 'ScoreIncreaseMods') {
-            result += mods[x] + ',';
-        }
-    }
-
-    result = result.substring(0, result.length - 1);
-    return result;
 }
 
 init();
 
-function init() {
+async function init() {
     try {
         let keyFile = require(KEYFILE);
         if (keyFile.key == "" || keyFile.key == undefined) return;
+        await auth.login(keyFile.id, keyFile.secret);
 
-        osu_api = new Api(keyFile.key, {
-            // baseUrl: sets the base api url (default: https://osu.ppy.sh/api)
-            notFoundAsError: true, // Throw an error on not found instead of returning nothing. (default: true)
-            completeScores: true, // When fetching scores also fetch the beatmap they are for (Allows getting accuracy) (default: false)
-            parseNumeric: false // Parse numeric values into numbers/floats, excluding ids
-        });
         console.log("osu! API is ready");
         valid = true;
     } catch (err) {
